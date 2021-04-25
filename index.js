b98898f13b6e1bc204bb0b3f02a3356c115faffa
pacificsuraj <stsurajst@gmail.com>
2021-04-25 11:15:22 +0545

    response added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", () => "app is running");
+app.get("/", (req, res) => {
+  return res.status(200).send("it is working normally");
+});
 
 app.listen(PORT, () => console.log(` server is running on PORT ${PORT}`));
