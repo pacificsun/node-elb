1766b652c5faeedffcf785784510924770110609
pacificsuraj <stsurajst@gmail.com>
2021-04-25 09:03:49 +0545

    get added

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('\', ()=> ("app is running "))
+
 app.listen(PORT, () => console.log(` server is running on PORT ${PORT}`));
